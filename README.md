a0b302772207765d611c19f8b938db8fc42b17f4
Jason Lengstorf <jason@lengstorf.com>
2023-08-04 15:40:48 -0700

    feat: SSO added

---
+++ README.md
@@ -0,0 +1,45 @@
+# WorkOS for Six-Figure SaaS Contracts
+
+Landing a six-figure contract as a SaaS company is a huge milestone. To make sure your company is ready to win the big deals, you'll need to be ready to meet enterprise-level expectations around user management:
+
+- SSO
+- Directory Sync
+- Audit Logging
+
+[WorkOS](https://lwj.dev/workos) makes it possible to set all of this up quickly in a fully self-serve workflow. No sales calls required!
+
+This demo app shows how to implement SSO for a Node app using WorkOS for SSO and Okta as an identity provider.
+
+## Local Dev
+
+### Prerequisites
+
+- A WorkOS account
+- An Okta account (the free developer preview is great for testing this out if you don't have an Okta account already)
+- Create a WorkOS connection to Okta
+- Create an Okta app to allow SAML 2.0 sign-in
+
+### Add environment variables
+
+Create a `.env` file with the following values:
+
+```bash
+SESSION_SECRET='minimum 32-character key for user sessions'
+WORKOS_API_KEY='your_workos_api_key'
+WORKOS_CLIENT_ID='your_workos_client_id'
+WORKOS_CONNECTION_ID='your_workos_connection_id'
+```
+
+### Install dependencies
+
+```bash
+npm i
+```
+
+### Start the dev server
+
+```bash
+npm run dev
+```
+
+Open `http://localhost:3000` to see the app and log in using your SSO connection.
---
+++ src/app.ts
@@ -0,0 +1,130 @@
+import "dotenv/config";
+
+import type { FastifyPluginAsync, RequestGenericInterface } from "fastify";
+import fastifyCookie from "@fastify/cookie";
+import fastifySession from "@fastify/session";
+import fastifyView from "@fastify/view";
+import fastifyStatic from "@fastify/static";
+import ejs from "ejs";
+import { WorkOS } from "@workos-inc/node";
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+declare module "fastify" {
+	interface Session {
+		user: {
+			id: string;
+			first_name?: string;
+			last_name?: string;
+			email: string;
+		};
+	}
+}
+
+interface StaticRequest extends RequestGenericInterface {
+	Params: {
+		filename: string;
+	};
+}
+
+interface RedirectRequest extends RequestGenericInterface {
+	Querystring: {
+		code: string;
+	};
+}
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const workos = new WorkOS(process.env.WORKOS_API_KEY);
+const clientID = process.env.WORKOS_CLIENT_ID!;
+const connection = process.env.WORKOS_CONNECTION_ID!;
+
+const app: FastifyPluginAsync = async (fastify, _opts) => {
+	// Fastify plugins and config
+	fastify.register(fastifyCookie);
+	fastify.register(fastifySession, {
+		secret: process.env.SESSION_SECRET!,
+		cookie:
+			process.env.NODE_ENV === "production"
+				? {
+						path: "/",
+						maxAge: 86400,
+						httpOnly: true,
+						secure: true,
+						sameSite: true,
+				  }
+				: { secure: false },
+	});
+
+	fastify.register(fastifyView, { engine: { ejs } });
+
+	fastify.register(fastifyStatic, {
+		root: path.join(__dirname, "..", "public"),
+	});
+
+	// WorkOS SSO flow
+	fastify.get("/auth/sso", async (_req, res) => {
+		const authorizationUrl = workos.sso.getAuthorizationURL({
+			connection,
+			clientID,
+			redirectURI: "http://localhost:3000/auth/sso/redirect",
+		});
+
+		res.redirect(authorizationUrl);
+	});
+
+	fastify.get<RedirectRequest>("/auth/sso/redirect", async (req, res) => {
+		const { code } = req.query;
+
+		const { profile } = await workos.sso.getProfileAndToken({
+			code,
+			clientID,
+		});
+
+		req.session.user = {
+			id: profile.id,
+			first_name: profile.first_name,
+			last_name: profile.last_name,
+			email: profile.email,
+		};
+
+		return res.redirect("/dashboard");
+	});
+
+	fastify.get("/auth/logout", async (req, reply) => {
+		req.session.destroy();
+		reply.redirect("/");
+	});
+
+	// App views
+	fastify.get("/", async (req, reply) => {
+		const user = req.session.user;
+
+		return reply.view("src/templates/index.ejs", {
+			user,
+		});
+	});
+
+	fastify.get("/dashboard", async (req, reply) => {
+		if (!req.session.user?.id) {
+			return reply.redirect("/auth/sso");
+		}
+
+		const user = req.session.user;
+
+		return reply.view("src/templates/dashboard.ejs", {
+			user,
+		});
+	});
+
+	// all other requests fall through to static files
+	fastify.get<StaticRequest>("/:filename", async function (request, reply) {
+		const { filename } = request.params;
+
+		return reply.sendFile(filename);
+	});
+};
+
+export default app;
+export { app };
