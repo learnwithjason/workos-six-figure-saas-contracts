8519a6964be87ac55f2e1c46ac87389c1a4f1174
Jason Lengstorf <jason@lengstorf.com>
2023-08-09 16:06:35 -0700

    feat: revert to starting point

--- src/app.ts
+++ src/app.ts
@@ -1,87 +1,33 @@
 import "dotenv/config";
 
 import type { FastifyPluginAsync } from "fastify";
-import type { StaticRequest, RedirectRequest } from "./types.js";
+import type { StaticRequest } from "./types.js";
 import * as path from "path";
 import { fileURLToPath } from "url";
 import ejs from "ejs";
 import fastifyView from "@fastify/view";
 import fastifyStatic from "@fastify/static";
-import fastifyCookie from "@fastify/cookie";
-import fastifySession from "@fastify/session";
-import { WorkOS } from "@workos-inc/node";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const publicDirRoot = path.join(__dirname, "..", "public");
 
-const workos = new WorkOS(process.env.WORKOS_API_KEY);
-
 const app: FastifyPluginAsync = async (fastify, _opts) => {
 	fastify.register(fastifyStatic, { root: publicDirRoot });
 	fastify.register(fastifyView, { engine: { ejs } });
-	fastify.register(fastifyCookie);
-	fastify.register(fastifySession, {
-		secret: process.env.SESSION_SECRET!,
-		cookie:
-			process.env.NODE_ENV === "production"
-				? {
-						path: "/",
-						maxAge: 86400,
-						httpOnly: true,
-						secure: true,
-						sameSite: true,
-				  }
-				: { secure: false },
-	});
-
-	// WorkOS SSO flow
-	fastify.get("/auth/sso", async (_req, res) => {
-		const authorizationUrl = workos.sso.getAuthorizationURL({
-			clientID: process.env.WORKOS_CLIENT_ID!,
-			organization: process.env.WORKOS_ORG_ID,
-			redirectURI: process.env.REDIRECT_URI!,
-		});
-
-		res.redirect(authorizationUrl);
-	});
-
-	fastify.get<RedirectRequest>("/auth/sso/redirect", async (req, res) => {
-		const { code } = req.query;
-		const { profile } = await workos.sso.getProfileAndToken({
-			code,
-			clientID: process.env.WORKOS_CLIENT_ID!,
-		});
-
-		req.session.user = {
-			id: profile.id,
-			first_name: profile.first_name,
-			last_name: profile.last_name,
-			email: profile.email,
-		};
-
-		return res.redirect("/dashboard");
-	});
-
-	fastify.get("/auth/logout", async (req, reply) => {
-		req.session.destroy();
-		reply.redirect("/");
-	});
 
 	// App views
 	fastify.get("/", async (req, reply) => {
 		return reply.view("src/templates/index.ejs", {
-			user: req.session.user,
+			user: undefined,
 		});
 	});
 
 	fastify.get("/dashboard", async (req, reply) => {
-		if (!req.session.user?.id) {
-			return reply.redirect("/auth/sso");
-		}
+		// TODO only show the dashboard if the user is logged in
 
 		return reply.view("src/templates/dashboard.ejs", {
-			user: req.session.user,
+			user: { first_name: "Guest" },
 		});
 	});
 
